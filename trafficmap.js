89e1ded21bef63851e9a7312cf97f7e1af2b259a
Steve Leathers <sayhello@steveleathers.com>
2015-05-04 11:14:31 -0700

    added legend text and toggle feature

--- trafficmap.js
+++ trafficmap.js
@@ -9,15 +9,50 @@ function initialize() {
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+  var url_1 = "http://steveleathers.github.io/sanbag-googlemap/sanbagPoints.kmz";
+  var layers = {
+    "layer_0": {
+      "layer" :  new google.maps.TrafficLayer(),
+      "label" : "Live Traffic"},
+    "layer_1": {
+      "layer" : new google.maps.KmlLayer(url_1),
+      "label" : "Sanbag Points"
+    };
   var sanbagPoints = new google.maps.KmlLayer({
     url: 'http://steveleathers.github.io/sanbag-googlemap/sanbagPoints.kmz'
   });
-  sanbagPoints.setMap(map);
 
-  
+  layers['layer_0']['layer'].setMap(map);
+  layers['layer_1']['layer'].setMap(map);
+
+  layers['layer_10']['layer'].toggleState = 1;
+  layers['layer_11']['layer'].toggleState = 1;
+
+  $('#layer-9-header').addClass('on');
+  $('#layer-10-header').addClass('on');
+
+  function toggleLayer(i) {
+    layer = layers[i]['layer'];
+    if (layer.toggleState == 1) {
+      layer.setMap(null);
+      layer.toggleState = 0;
+    }
+    else {
+      layer.setMap(map);
+      layer.toggleState = 1;
+    }
+  }
+
+  $('#layer-0-header').click( function() {
+    $('#layer-0-header').toggleClass('on');
+    toggleLayer('layer_0');
+  } );
+
+  $('#layer-1-header').click( function() {
+    $('#layer-1-header').toggleClass('on');
+    toggleLayer('layer_1');
+  } );
+
   map.controls[google.maps.ControlPosition.RIGHT_TOP].push(
   document.getElementById('legend'));
 }
